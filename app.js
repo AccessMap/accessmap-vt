2950254224a45de26aa92f7fd074bc1731e151f5
Nick Bolten <nbolten@gmail.com>
2016-12-21 00:15:32 -0700

    Added construction layer

--- app.js
+++ app.js
@@ -5,9 +5,15 @@ var app = new Tilesplash(process.env.DATABASE_URL);
 app.server.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader('Content-Type', 'application/x-protobuf')
   next();
 });
 
+// FIXME: tilesplash will return an empty tile with status 200 if there's no
+// data in the query (e.g. zooming to a region outside the data's extent).
+// This happens because there is basically zero data checking after the SQL
+// query has been turned into GeoJSON -> mapnik VectorTile. What should a
+// 'real' empty tile look like? Should we switch to another library?
 app.layer('seattle', function(tile, render) {
   render({
     sidewalks: 'SELECT ST_AsGeoJSON(geom) AS the_geom_geojson, \
@@ -19,6 +25,18 @@ app.layer('seattle', function(tile, render) {
                        curbramps \
                   FROM crossings \
                  WHERE ST_Intersects(geom, !bbox_4326!)',
+  });
+});
+
+app.layer('live', function(tile, render) {
+  render({
+    construction: 'SELECT ST_AsGeoJSON(geom) AS the_geom_geojson, \
+                          address, \
+                          to_char(start_date, \'YYYY-MM-DD\') AS start_date, \
+                          to_char(end_date, \'YYYY-MM-DD\') AS end_date, \
+                          closed \
+                     FROM construction \
+                    WHERE ST_Intersects(geom, !bbox_4326!)'
   });
 });
 
